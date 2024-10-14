99f74e741de821ae7f3af5ca2b251acc327e2143
10-5 <whrjswo123@naver.com>
2024-10-14 14:33:46 +0900

    V 1.0.0 - 화면 처리 기본 / 페이징 처리

---
+++ src/main/java/org/zerock/guestbook/dto/PageResultDTO.java
@@ -0,0 +1,74 @@
+package org.zerock.guestbook.dto;
+
+import lombok.Data;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Data
+public class PageResultDTO<DTO, EN> {
+    // 리스트 응답시 페이징
+    // 리스트 처리 결과를 Page<Entity> 타입으로 변환
+    // 화면에 출력에 필요한 페이지 정보를 구성
+
+    private List<DTO> dtoList; // 제너릭으로 받은 dto를 리스트 처리
+    
+    // 총 페이지 번호
+    private int totalPage ;
+
+    // 현재 페이지 번호
+    private int page ;
+
+    // 목록의 사이즈
+    private int size ;
+
+    // 시작 페이지번호, 끝 페이지 번호
+    private int start, end ;
+
+    // 이전, 다음 버튼
+    private boolean prev, next ;
+
+    // 페이지 번호 목록
+    private List<Integer> pageList ;  // 1, 2, 3, 4, 5.... 11, 12, 13, ....
+
+
+    public PageResultDTO(Page<EN> result, Function<EN,DTO> fn){ // 엔티티를 함수형으로 처리
+        //Function<EN, DTO> fn -> Entity 객체드을 dto로 변환해 주는 기능
+        // 나중에 어떤 종류의 Page<E> 타입이 생성되더라도, 처리 가능함.
+        dtoList = result.stream().map(fn).collect(Collectors.toList());
+
+        // 추가
+
+        totalPage = result.getTotalPages(); // 코배스부교재 p.68 참고 (총 페이지)
+        
+        makePageList(result.getPageable()); // 아래쪽에 메서드 생성
+        
+    }
+
+    private void makePageList(Pageable pageable){
+        // 페이지 리스트 생성용 전용 코드
+
+        this.page = pageable.getPageNumber() + 1 ; // 0 부터 시작 함.
+        this.size = pageable.getPageSize();
+        
+        // 계산용 페이징
+        int tempEnd = (int)(Math.ceil(page/10.0)) * 10 ;
+
+        start = tempEnd - 9;
+        prev = start > 1 ; // 시작 페이지가 1보다 크면 있음
+        end = totalPage > tempEnd ? tempEnd : totalPage ;
+        //           condition       true       false
+
+        next = totalPage > tempEnd ; // next 버튼 (다음 페이지 용 코드)
+
+        pageList = IntStream.rangeClosed(start, end).boxed().collect(Collectors.toList());
+        // 페이징용 가로 리스트 결과   prev(t/f) 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 next(t/f)
+
+
+
+    }
+}
---
+++ src/test/java/org/zerock/guestbook/repository/GuestBookTests.java
@@ -0,0 +1,221 @@
+package org.zerock.guestbook.repository;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.zerock.guestbook.entity.Guestbook;
+import org.zerock.guestbook.entity.QGuestbook;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class GuestBookTests {
+
+    @Autowired
+    private GuestbookRepository guestbookRepository;
+
+    @Test
+    public void insertDummies(){
+        // 300개 정도 입력 테스트
+
+        IntStream.rangeClosed(1,300).forEach(i -> {
+            Guestbook guestbook = Guestbook.builder()
+                    .title("제목...." + i)
+                    .content("내용...." + i)
+                    .writer("user" + (i%10)) // user0....user1....user2....user3....
+                    .build();
+            System.out.println(guestbookRepository.save(guestbook));
+            // repository에 jpa 내장된 메서드인 save로 insert 처리함.
+        });
+    }
+
+    @Test
+    public void updateTest(){
+
+        Optional<Guestbook> result = guestbookRepository.findById(300L);
+        // select * from guestbook where gno=300
+
+        if(result.isPresent()){ //Optional 값이 null이 아니면
+            Guestbook guestbook = result.get(); // 검색한 300 객체를 가져와 guestbook에 넣음
+            guestbook.changeTitle("수정된 제목.....");
+            guestbook.changeContent("수정된 내용.....");
+            guestbookRepository.save(guestbook);
+            //save -> update set ........
+            
+
+        }
+    }
+            //    Hibernate:
+            //    select
+            //    g1_0.gno,
+            //    g1_0.content,
+            //    g1_0.moddate,
+            //    g1_0.regdate,
+            //    g1_0.title,
+            //    g1_0.writer
+            //            from
+            //    guestbook g1_0
+            //    where
+            //    g1_0.gno=?
+            //    Hibernate:
+            //    select
+            //    g1_0.gno,
+            //    g1_0.content,
+            //    g1_0.moddate,
+            //    g1_0.regdate,
+            //    g1_0.title,
+            //    g1_0.writer
+            //            from
+            //    guestbook g1_0
+            //    where
+            //    g1_0.gno=?
+            //    Hibernate:
+            //    update
+            //            guestbook
+            //    set
+            //    content=?,
+            //    moddate=?,
+            //    title=?,
+            //    writer=?
+            //    where
+            //    gno=?
+
+    @Test
+    public void testQuery(){
+        // 쿼리 dsl를 이용해서 단일 검색용 -> 0페이지, 10개, 정렬(gno, 내림), 제목=1  들어 있는 조건
+
+        Pageable pageable = PageRequest.of(0,10, Sort.by("gno").descending());
+        // 0페이지, 10개, 정렬(gno.내림)
+
+        QGuestbook qGuestbook = QGuestbook.guestbook; // 쿼리dsl로 객체 생성
+
+        String keyword = "1";
+
+        BooleanBuilder builder = new BooleanBuilder(); // where 조건이 있냐? 없냐?
+
+        BooleanExpression expression = qGuestbook.title.contains(keyword); // 제목에 1을 넣음.
+        // 주의 import com.querydsl.core.types.dsl.BooleanExpression;
+        
+        builder.and(expression); // 검색 where 문 붙임.
+        
+        Page<Guestbook> result = guestbookRepository.findAll(builder,pageable); // 찾은 값을 적용
+        // builder -> where, pageable -> 페이징+정렬
+        
+        result.stream().forEach(guestbook -> {
+            System.out.println(guestbook);
+        }); // 결과 콘솔 출력
+
+            //        Hibernate:
+            //        select
+            //        g1_0.gno,
+            //                g1_0.content,
+            //                g1_0.moddate,
+            //                g1_0.regdate,
+            //                g1_0.title,
+            //                g1_0.writer
+            //        from
+            //        guestbook g1_0
+            //        where
+            //        g1_0.title like ? escape '!'
+            //        order by
+            //        g1_0.gno desc
+            //        limit
+            //                ?, ?
+            //        Hibernate:
+            //        select
+            //        count(g1_0.gno)
+            //        from
+            //        guestbook g1_0
+            //        where
+            //        g1_0.title like ? escape '!'
+            //        Guestbook(gno=291, title=제목....291, content=내용....291, writer=user1)
+            //        Guestbook(gno=281, title=제목....281, content=내용....281, writer=user1)
+            //        Guestbook(gno=271, title=제목....271, content=내용....271, writer=user1)
+            //        Guestbook(gno=261, title=제목....261, content=내용....261, writer=user1)
+            //        Guestbook(gno=251, title=제목....251, content=내용....251, writer=user1)
+            //        Guestbook(gno=241, title=제목....241, content=내용....241, writer=user1)
+            //        Guestbook(gno=231, title=제목....231, content=내용....231, writer=user1)
+            //        Guestbook(gno=221, title=제목....221, content=내용....221, writer=user1)
+            //        Guestbook(gno=219, title=제목....219, content=내용....219, writer=user9)
+            //        Guestbook(gno=218, title=제목....218, content=내용....218, writer=user8)
+
+    }
+
+
+    @Test
+    public void testQueryMulti(){
+        // 제목과 내용을 where 문으로 다중검색
+
+        Pageable pageable = PageRequest.of(0,7, Sort.by("gno").descending());
+
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+
+        String keyword = "2";
+
+        BooleanBuilder builder = new BooleanBuilder(); // 조건이 있냐? where 생성용
+
+        BooleanExpression exTitle = qGuestbook.title.contains(keyword); // 조건 1
+        BooleanExpression exContent = qGuestbook.content.contains(keyword); // 조건 2
+
+        // 조건들을 합체
+        BooleanExpression exAll = exTitle.or(exContent); // where title or content
+
+        builder.and(exAll); // 조건문 합체
+
+        builder.and(qGuestbook.gno.gt(0L)); // pk에 index를 활용해서 빠른 추출
+
+        Page<Guestbook> result = guestbookRepository.findAll(builder,pageable);
+
+        result.stream().forEach(guestbook -> {
+            System.out.println(guestbook);
+
+        });
+
+    }
+            //    Hibernate:
+            //    select
+            //    g1_0.gno,
+            //    g1_0.content,
+            //    g1_0.moddate,
+            //    g1_0.regdate,
+            //    g1_0.title,
+            //    g1_0.writer
+            //            from
+            //    guestbook g1_0
+            //    where
+            //            (
+            //                    g1_0.title like ? escape '!'
+            //                    or g1_0.content like ? escape '!'
+            //            )
+            //    and g1_0.gno>?
+            //    order by
+            //    g1_0.gno desc
+            //    limit
+            //        ?, ?
+            //    Hibernate:
+            //    select
+            //    count(g1_0.gno)
+            //    from
+            //    guestbook g1_0
+            //    where
+            //            (
+            //                    g1_0.title like ? escape '!'
+            //                    or g1_0.content like ? escape '!'
+            //            )
+            //    and g1_0.gno>?
+            //    Guestbook(gno=299, title=제목....299, content=내용....299, writer=user9)
+            //    Guestbook(gno=298, title=제목....298, content=내용....298, writer=user8)
+            //    Guestbook(gno=297, title=제목....297, content=내용....297, writer=user7)
+            //    Guestbook(gno=296, title=제목....296, content=내용....296, writer=user6)
+            //    Guestbook(gno=295, title=제목....295, content=내용....295, writer=user5)
+            //    Guestbook(gno=294, title=제목....294, content=내용....294, writer=user4)
+            //    Guestbook(gno=293, title=제목....293, content=내용....293, writer=user3)
+
+}
